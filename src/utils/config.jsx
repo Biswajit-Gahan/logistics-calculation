6361d6e7582ab18fc073685557535cce1de0df8d
biswajit gahan <dev.infoshare@gmail.com>
2025-07-20 11:15:37 +0530

    base url change

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,6 +1,6 @@
 const config = {
     api: {
-        baseUrl: "https://logistics-calculation-proxy.netlify.app/",
+        baseUrl: "https://logistics-calculation-proxy.netlify.app",
         getAllVehicleEndpoint: "/api/all-vehicles",
         saveCalculationHistoryEndpoint: "/api/save-history",
     }
